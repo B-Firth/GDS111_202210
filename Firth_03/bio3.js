35ffb6944375169753df4fa89c11ccfa64caf051
Branden Firth <brfirth@email.neit.edu>
2021-10-30 14:07:32 -0400

    Bio assignment 3 | UPDATE | Bio3 complete - 10/30/2021...

---
+++ Firth_03/bio3.js
@@ -0,0 +1,65 @@
+var an1 = "Your Name!" //answer to question 1
+
+var an2 = "the Post Office!" //answer to question 2
+
+var an3 = "NOON" // answer to question3
+
+var an4 = "An Echo" // answer to question4
+
+var an5 = "A Candle" // answer to question5
+
+var an6 = "A Map" // answer to question6
+
+var an7 = "The letter: R" // answer to question7
+
+var an8 = "All the people were married" // answer to question8
+
+var an9 = "Bookkeeper" // answer to question9
+
+var an10 = "She took a picture of him and developed it in her darkroom." // answer to question10
+
+
+//Functions - perform tasks; the have set of processes assigned to them, and when they are CALLED their tasks are performed
+
+//function below will allow the buttons to work by displating the answers
+
+function answer1() {
+
+    document.getElementById("question1").innerHTML = an1;
+}
+
+function answer2() {
+    document.getElementById("question2").innerHTML = an2;
+}
+
+function answer3() {
+    document.getElementById("question3").innerHTML = an3;
+}
+
+function answer4() {
+    document.getElementById("question4").innerHTML = an4;
+}
+
+function answer5() {
+    document.getElementById("question5").innerHTML = an5;
+}
+
+function answer6() {
+    document.getElementById("question6").innerHTML = an6;
+}
+
+function answer7() {
+    document.getElementById("question7").innerHTML = an7;
+}
+
+function answer8() {
+    document.getElementById("question8").innerHTML = an8;
+}
+
+function answer9() {
+    document.getElementById("question9").innerHTML = an9;
+}
+
+function answer10() {
+    document.getElementById("question10").innerHTML = an10;
+}
