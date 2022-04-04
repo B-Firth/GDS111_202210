3a63403530327995eda0f6716dcf0bce08d32e9f
Branden Firth <brfirth@email.neit.edu>
2022-04-03 23:11:05 -0400

    UPDATE | BOSS LEVEL FINISHED | UPDATE COMPLETE...

--- gds137/Week1/js/Player.js
+++ gds137/Week1/js/Player.js
@@ -14,7 +14,7 @@ function Circle()
 	this.vy = 0;
 	
 	//player's color
-	this.color = "#551A8B";
+	this.color = "#0000FF";
 
 	this.force = 1;
 	
@@ -45,6 +45,91 @@ function Circle()
 	
 	//This changes the player's position
 	this.move = function()
+	{
+		this.x += this.vx;
+		this.y += this.vy;
+	}
+}
+
+
+
+
+function Circle2()
+{
+	//player's location
+	this.x = 50
+	this.y = 60;
+	
+	//player's dimensions
+	this.width = 50;
+	this.height = 50;
+	
+	//player's velocity or speed on each axis
+	this.vx = 10;
+	this.vy = 10;
+	
+	//player's color
+	this.color = "#00FFFF";
+
+	this.force = 1;
+
+	this.drawCircle2 = function()
+	{
+		context.save();
+		context.fillStyle = this.color;
+			context.translate(this.x, this.y);
+		context.beginPath();
+		context.arc(0,0,this.width/2,0,360*Math.PI/180,true);
+		context.closePath();
+		context.fill();
+		context.restore();
+		
+	}	
+	
+	//This changes the player's position
+	this.move2 = function()
+	{
+		this.x += this.vx;
+		this.y += this.vy;
+	}
+}
+
+
+
+function Circle3()
+{
+	//player's location
+	this.x = 150
+	this.y = 150;
+	
+	//player's dimensions
+	this.width = 250;
+	this.height = 250;
+	
+	//player's velocity or speed on each axis
+	this.vx = 2;
+	this.vy = 2;
+	
+	//player's color
+	this.color = "#551A8B";
+
+	this.force = 1;
+
+	this.drawCircle3 = function()
+	{
+		context.save();
+		context.fillStyle = this.color;
+			context.translate(this.x, this.y);
+		context.beginPath();
+		context.arc(0,0,this.width/2,0,360*Math.PI/180,true);
+		context.closePath();
+		context.fill();
+		context.restore();
+		
+	}	
+	
+	//This changes the player's position
+	this.move3 = function()
 	{
 		this.x += this.vx;
 		this.y += this.vy;
--- gds137/Week1/js/game.js
+++ gds137/Week1/js/game.js
@@ -9,10 +9,18 @@ var player;
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
 	player = new Circle();
+	player2 = new Circle2();
+	player3 = new Circle3();
 	
 	//------Declare the Player's speed on the x and y axis------
-	player.vx = 2;
-	player.vy = 2;
+	player.vx = 3;
+	player.vy = 3;
+
+	player2.vx = 5;
+	player2.vy = 5;
+
+	player3.vx = 1;
+	player3.vy = 1;
 	//----------------------------------------------------
 	
 	timer = setInterval(animate, interval);
@@ -24,6 +32,8 @@ function animate()
 	
 	//----Movement Using the Player's move() function----
 	player.move();
+	player2.move2();
+	player3.move3();
 	//---------------------------------------------------
 	
 	//--------------Bounce of Right----------------------
@@ -31,13 +41,32 @@ function animate()
 	{
 		player.x = canvas.width - player.width/2
 		player.vx = -player.vx;	
-		player.vx = player.vx - 1
+		player.vx = player.vx - 2;
+		if(player.vx > 50)
+		{
+			player.vx = 5
+		}
+	}
+
+		if(player.x > canvas.width - player.width/2)
+	{
+		player.x = canvas.width - player.width/2
+		player.vx = -player.vx;	
+		player.vx = player.vx - 2;
+		if(player.vx > 50)
+		{
+			player.vx = 5
+		}
 	}
 
 	if(player.x < 0 + player.width/2)
 	{
 		player.vx = -player.vx;	
-		player.vx = player.vx + 1
+		player.vx = player.vx + 2
+		if(player.vx > 50)
+		{
+			player.vx = 5
+		}
 	}
 
 
@@ -45,15 +74,140 @@ function animate()
 	{
 		player.y = canvas.height - player.height/2
 		player.vy = -player.vy;
-		player.vy = player.vy - 1
+		player.vy = player.vy - 2
+		if(player.vy > 50)
+		{
+			player.vy = 5
+		}
 	}
 
 	if(player.y < 0 + player.height/2)
 	{
 		player.vy = -player.vy;	
-		player.vy = player.vy + 1
+		player.vy = player.vy + 2
+		if(player.vy > 50)
+		{
+			player.vy = 5
+		}
 	}
+	
+
+	//----Player2-----------------------------------------------
+
+	if(player2.x > canvas.width - player2.width/2)
+	{
+		player2.x = canvas.width - player2.width/2
+		player2.vx = -player2.vx;	
+		player2.vx = player2.vx - 5;
+		if(player2.vx > 100)
+		{
+			player2.vx = 10
+		}
+	}
+
+		if(player2.x > canvas.width - player2.width/2)
+	{
+		player2.x = canvas.width - player2.width/2
+		player2.vx = -player2.vx;	
+		player2.vx = player2.vx - 5;
+		if(player2.vx > 100)
+		{
+			player2.vx = 10
+		}
+	}
+
+	if(player2.x < 0 + player2.width/2)
+	{
+		player2.vx = -player2.vx;	
+		player2.vx = player2.vx + 5
+		if(player2.vx > 100)
+		{
+			player2.vx = 10
+		}
+	}
+
+
+	if(player2.y > canvas.height - player2.height/2)
+	{
+		player2.y = canvas.height - player2.height/2
+		player2.vy = -player2.vy;
+		player2.vy = player2.vy - 5
+		if(player2.vy > 100)
+		{
+			player2.vy = 10
+		}
+	}
+
+	if(player2.y < 0 + player2.height/2)
+	{
+		player2.vy = -player2.vy;	
+		player2.vy = player2.vy + 5
+		if(player2.vy > 100)
+		{
+			player2.vy = 10
+		}
+	}
+
+
+	//---------------Player3-----------------------------
+
+	if(player3.x > canvas.width - player3.width/2)
+	{
+		player3.x = canvas.width - player3.width/2
+		player3.vx = -player3.vx;	
+		player3.vx = player3.vx - 1;
+		if(player3.vx > 20)
+		{
+			player3.vx = 1
+		}
+	}
+
+		if(player3.x > canvas.width - player3.width/2)
+	{
+		player3.x = canvas.width - player3.width/2
+		player3.vx = -player3.vx;	
+		player3.vx = player3.vx - 1;
+		if(player3.vx > 20)
+		{
+			player3.vx = 1
+		}
+	}
+
+	if(player3.x < 0 + player3.width/2)
+	{
+		player3.vx = -player3.vx;	
+		player3.vx = player3.vx + 1
+		if(player3.vx > 20)
+		{
+			player3.vx = 1
+		}
+	}
+
+
+	if(player3.y > canvas.height - player3.height/2)
+	{
+		player3.y = canvas.height - player3.height/2
+		player3.vy = -player3.vy;
+		player3.vy = player3.vy - 1
+		if(player3.vy > 20)
+		{
+			player3.vy = 1
+		}
+	}
+
+	if(player3.y < 0 + player3.height/2)
+	{
+		player3.vy = -player3.vy;	
+		player3.vy = player3.vy + 1
+		if(player3.vy > 20)
+		{
+			player3.vy = 1
+		}
+	}
+
 	//---------------------------------------------------
 	
+	player3.drawCircle3();
 	player.drawCircle();
+	player2.drawCircle2();
 }
